8ed7dc934d515a25ef59c29474bcfca516dc6205
tiketora <117682007+tiketora@users.noreply.github.com>
2026-04-17 01:21:08 +0200

    Resizing logo

--- components/tetto94/navbar.tsx
+++ components/tetto94/navbar.tsx
@@ -180,9 +180,9 @@ export default function Navbar() {
             <motion.div
               whileHover={{ scale: 1.03 }}
               transition={{ type: 'spring', stiffness: 400, damping: 20 }}
-              className="flex items-center h-16 md:h-20"
+              className="flex items-center h-24 md:h-20"
             >
-              <Tetto94Logo className="h-16 md:h-20 w-auto" />
+              <Tetto94Logo className="h-24 md:h-20 w-auto" />
             </motion.div>
           </a>
 
